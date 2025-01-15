2fe5fb545f0fe4cbf92904d5907111d09eab94e7
zinkevish <zinkevish77@gmail.com>
2025-01-15 18:27:25 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -17,8 +17,39 @@ class House:
     def __len__(self):
         return self.number_of_floors
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 5)
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        return House (self.name, self.number_of_floors + value)
+
+    def __radd__(self, value):
+        return House (self.name, self.number_of_floors + value)
+
+    def __iadd__(self, value):
+        return House (self.name, self.number_of_floors + value)
+
+
+
+
+h1 = House('ЖК Горский', 10)
+h2 = House('Домик в деревне', 20)
 h1.go_to(20)
 h2.go_to(4)
 
@@ -26,6 +57,25 @@ h2.go_to(4)
 print(h1)
 print(h2)
 
-# __len__
-print(len(h1))
-print(len(h2))
+# # __len__
+# print(len(h1))
+# print(len(h2))
+
+print(h1 == h2)
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
